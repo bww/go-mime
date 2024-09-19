e24d0bd58f30d1c500218b83b5a7bd7427e93f93
Brian W. Wolter <brianwolter@gmail.com>
2024-09-19 09:46:15 -0400

    Improving the handling of parameters

--- v1/format_test.go
+++ v1/format_test.go
@@ -1,6 +1,7 @@
 package mime
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
@@ -9,24 +10,27 @@ import (
 
 func TestType(t *testing.T) {
 	tests := []struct {
-		In     string
-		Type   Type
-		Params map[string]string
-		Err    func(error) error
+		In         string
+		Type, Base Type
+		Params     map[string]string
+		Err        func(string, error) error
 	}{
 		{
 			In:     "text/plain",
 			Type:   Type("text/plain"),
+			Base:   Type("text/plain"),
 			Params: map[string]string{},
 		},
 		{
 			In:     "text/plain+json",
 			Type:   Type("text/plain+json"),
+			Base:   Type("text/plain+json"),
 			Params: map[string]string{},
 		},
 		{
 			In:   "text/plain+json;charset=utf8",
 			Type: Type("text/plain+json;charset=utf8"),
+			Base: Type("text/plain+json"),
 			Params: map[string]string{
 				"charset": "utf8",
 			},
@@ -34,6 +38,7 @@ func TestType(t *testing.T) {
 		{
 			In:   "text/plain+json; charset=utf8",
 			Type: Type("text/plain+json;charset=utf8"),
+			Base: Type("text/plain+json"),
 			Params: map[string]string{
 				"charset": "utf8",
 			},
@@ -41,9 +46,20 @@ func TestType(t *testing.T) {
 		{
 			In:   "text/plain+json; charset=utf8; alabama=state",
 			Type: Type("text/plain+json;alabama=state;charset=utf8"),
+			Base: Type("text/plain+json"),
 			Params: map[string]string{
 				"charset": "utf8",
 				"alabama": "state",
+			},
+		},
+		{
+			In: "text?plain+json; charset=utf8; alabama=state",
+			Err: func(str string, err error) error {
+				if err != nil {
+					return nil
+				} else {
+					return errors.New("Expected an error: " + str)
+				}
 			},
 		},
 	}
@@ -51,9 +67,10 @@ func TestType(t *testing.T) {
 		mt, p, err := Parse(e.In)
 		fmt.Printf("%s → %v %v; %v\n", e.In, mt, p, err)
 		if e.Err != nil {
-			assert.NoError(t, e.Err(err))
+			assert.NoError(t, e.Err(e.In, err))
 		} else if assert.NoError(t, err) {
 			assert.Equal(t, e.Type, mt, "#%d", i)
+			assert.Equal(t, e.Base, mt.Base(), "#%d", i)
 			assert.Equal(t, e.Params, p, "#%d", i)
 		}
 	}
